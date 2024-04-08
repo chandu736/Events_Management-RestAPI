fefc9b826600960ce35b9aeca049bae9da569af1
Chandu kousik Ponnaganti <108112556+chandu736@users.noreply.github.com>
2024-04-08 18:08:18 +0530

    Update EventController.java

--- Events-Finder-Rest-API/Events-Finder-Rest-API/src/main/java/com/restapi/EventsFinderRestAPI/Controller/EventController.java
+++ Events-Finder-Rest-API/Events-Finder-Rest-API/src/main/java/com/restapi/EventsFinderRestAPI/Controller/EventController.java
@@ -1,25 +1,34 @@
 package com.restapi.EventsFinderRestAPI.Controller;
 
-import java.util.List;
-
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.restapi.EventsFinderRestAPI.Model.EventRequestDTO;
-import com.restapi.EventsFinderRestAPI.Model.EventResponseDTO;
+import com.restapi.EventsFinderRestAPI.Exception.InvalidInputException;
+import com.restapi.EventsFinderRestAPI.Model.PaginatedEventResponseDTO;
 import com.restapi.EventsFinderRestAPI.Service.EventService;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/events")
 public class EventController {
+	
 	@Autowired
 	private EventService eventService;
 	
-	@GetMapping("events/find")
-	public List<EventResponseDTO> findEvents(EventRequestDTO request){
-		List<EventResponseDTO> events=eventService.findEvents(request.getUserLatitude(),request.getUserLongitude(),request.getDate());
-		return events;
+	@GetMapping("/find")
+	public PaginatedEventResponseDTO getEvents(
+	        @RequestParam double latitude,
+	        @RequestParam double longitude,
+	        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+	        @RequestParam(defaultValue = "1") int page,
+	        @RequestParam(defaultValue = "10") int pageSize) {
+	    if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180) {
+	        throw new InvalidInputException("Latitude and longitude must be within valid ranges.");
+	    }
+	    
+	    return eventService.getEventsWithWeatherAndDistance(latitude, longitude, startDate, page, pageSize);
 	}
 }
