b97af5a5f8a2f94e07b29637ad249c49fd97ddee
Chandu kousik Ponnaganti <108112556+chandu736@users.noreply.github.com>
2024-04-08 18:10:19 +0530

    Update EventResponseDTO.java

--- Events-Finder-Rest-API/Events-Finder-Rest-API/src/main/java/com/restapi/EventsFinderRestAPI/Model/EventResponseDTO.java
+++ Events-Finder-Rest-API/Events-Finder-Rest-API/src/main/java/com/restapi/EventsFinderRestAPI/Model/EventResponseDTO.java
@@ -3,67 +3,56 @@ package com.restapi.EventsFinderRestAPI.Model;
 import java.time.LocalDate;
 
 public class EventResponseDTO {
+	
 	private String eventName;
 	private String cityName;
 	private LocalDate date;
 	private String weather;
-	private Double distance;
-	
-	public EventResponseDTO() {
-		
+	private double distance;
+	@Override
+	public String toString() {
+		return "EventResponseDTO [eventName=" + eventName + ", cityName=" + cityName + ", date=" + date + ", weather="
+				+ weather + ", distance=" + distance + "]";
 	}
-	public EventResponseDTO(String eventName, String cityName, LocalDate date, String weather, Double distance) {
+	public EventResponseDTO(String eventName, String cityName, LocalDate date, String weather, double distance) {
+		super();
 		this.eventName = eventName;
 		this.cityName = cityName;
 		this.date = date;
 		this.weather = weather;
 		this.distance = distance;
 	}
-
+	public EventResponseDTO() {
+		// TODO Auto-generated constructor stub
+	}
 	public String getEventName() {
 		return eventName;
 	}
-
 	public void setEventName(String eventName) {
 		this.eventName = eventName;
 	}
-
 	public String getCityName() {
 		return cityName;
 	}
-
 	public void setCityName(String cityName) {
 		this.cityName = cityName;
 	}
-
 	public LocalDate getDate() {
 		return date;
 	}
-
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
-
 	public String getWeather() {
 		return weather;
 	}
-
 	public void setWeather(String weather) {
-		this.weather = weather; 
+		this.weather = weather;
 	}
-
-	public Double getDistance() {
+	public double getDistance() {
 		return distance;
 	}
-
-	public void setDistance(Double distance) {
+	public void setDistance(double distance) {
 		this.distance = distance;
 	}
-
-	@Override
-	public String toString() {
-		return "EventResponseDTO [eventName=" + eventName + ", cityName=" + cityName + ", date=" + date + ", weather="
-				+ weather + ", distance=" + distance + "]";
-	}
-	
 }
